8568dc34e4da96b31571a79550703377f18ea9d8
liorMar <lior.marzouk@gmail.com>
2017-07-20 10:41:34 +0300

    Reorder the stocks using the arrow controls

--- main.js
+++ main.js
@@ -23,78 +23,80 @@ const stocks =
     }
   ];
 
-// {
-//   let t = function () {
-//     function loadStocks(stocks) {
-//     const ulElement = document.querySelector('main > ul');
-//       ulElement.innerHTML = stocks.reduce(
-//         function (html, stock, index) {
-//           return html + addStock(stock, ulElement, index);
-//     }, '');
-//   }
-//
-//   function round(number) {
-//     return Math.round(number * 100) / 100;
-//   }
-//
-//   function redOrGreen(num) {
-//     if (num >= 0) {
-//       return 'green-button';
-//     } else
-//       return 'red-button';
-//   }
-//
-//   function addStock(stock, index) {
-//       return '<li position="'+index+'" class="box">' +
-//       '   <h2>'+stock.Symbol+' ('+stock.Name+'</h2>' +
-//       '   <div>' +
-//       '     ' +round(stock.LastTradePriceOnly)+
-//       '     <button class="change-button '+redOrGreen(Number(stock.PercentChange.substr(0, stock.PercentChange.length-1)))+'">'+stock.PercentChange+'</button>' +
-//       '     <div class="box-arrow">' +
-//       '       <span class="icon-arrow up-button"></span>' +
-//       '       <span class="icon-arrow down-button"></span>' +
-//       '     </div>' +
-//       '   </div>' +
-//       '</li>';
-//   }
-//
-//   loadStocks(stocks);
-//   }();
-// }
-
 (function init() {
   function loadStocks(stocks) {
     const ulElement = document.querySelector('main > ul');
     ulElement.innerHTML = stocks.reduce(
       function (html, stock, index) {
-        return html + addStock(stock, ulElement, index);
+        return html + addStock(stock, (index === 0 && 'disabled' ) || '', (index === (stocks.length - 1) && 'disabled' ) || '');
       }, '');
   }
 
   function round(number) {
-    return Math.round(number * 100) / 100;
+    return (Math.round(number * 100) / 100).toFixed(2);
   }
 
   function redOrGreen(num) {
-    if (num >= 0) {
-      return 'green-button';
-    } else
-      return 'red-button';
+    return num >= 0 ? 'green-button' : 'red-button';
   }
 
-  function addStock(stock, index) {
-    return '<li position="'+index+'" class="box">' +
-      '   <h2>'+stock.Symbol+' ('+stock.Name+'</h2>' +
-      '   <div>' +
-      '     ' +round(stock.LastTradePriceOnly)+
-      '     <button class="change-button '+redOrGreen(Number(stock.PercentChange.substr(0, stock.PercentChange.length-1)))+'">'+stock.PercentChange+'</button>' +
-      '     <div class="box-arrow">' +
-      '       <span class="icon-arrow up-button"></span>' +
-      '       <span class="icon-arrow down-button"></span>' +
-      '     </div>' +
-      '   </div>' +
-      '</li>';
+  function addStock(stock, upDisabled, downDisabled) {
+    return `<li class="box">
+      <h2>${stock.Symbol} (${stock.Name}</h2>   
+      <div>
+        ${round(stock.LastTradePriceOnly)}     
+        <button class="change-button ${redOrGreen(Number(stock.PercentChange.substr(0, stock.PercentChange.length - 1)))}">
+          ${stock.PercentChange} 
+        </button>     
+        <div class="box-arrow" data-symbol="${stock.Symbol}">       
+          <button class="icon-arrow up-button" ${upDisabled}></button>
+          <button class="icon-arrow down-button" ${downDisabled}></button>
+        </div>
+      </div>
+    </li>`;
   }
 
   loadStocks(stocks);
+
+  setHandler('.up-button', stockHandleUp);
+  setHandler('.down-button', stockHandleDown);
+
+  function stockHandleDown(ev) {
+    let symbol = ev.target.parentElement.getAttribute('data-symbol');
+
+    let stockIndex = 0;
+    stocks.find((stock, index) => {stockIndex = index; return stock.Symbol === symbol});
+
+    let tempStock = stocks[stockIndex];
+    stocks[stockIndex] = stocks[stockIndex+1];
+    stocks[stockIndex+1] = tempStock;
+
+    loadStocks(stocks);
+
+    setHandler('.up-button', stockHandleUp);
+    setHandler('.down-button', stockHandleDown);
+  }
+
+  function stockHandleUp(ev) {
+    let symbol = ev.target.parentElement.getAttribute('data-symbol');
+
+    let stockIndex = 0;
+    stocks.find((stock, index) => {stockIndex = index; return stock.Symbol === symbol});
+
+    let tempStock = stocks[stockIndex];
+    stocks[stockIndex] = stocks[stockIndex-1];
+    stocks[stockIndex-1] = tempStock;
+
+    loadStocks(stocks);
+
+    setHandler('.up-button', stockHandleUp);
+    setHandler('.down-button', stockHandleDown);
+  }
+
+  function setHandler(selector, handler) {
+    document.querySelectorAll(selector)
+      .forEach(function (li) {
+        li.addEventListener('click', handler);
+      })
+  }
 }());
